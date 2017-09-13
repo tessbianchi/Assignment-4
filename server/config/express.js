e0548b498873266c720a2988283845dfd17017d4
Tess <tesshbianchi@gmail.com>
2017-09-13 18:01:42 -0400

    Assignment 4

--- server/config/express.js
+++ server/config/express.js
@@ -1,37 +1,35 @@
-var path = require('path'),  
-    express = require('express'), 
-    mongoose = require('mongoose'),
-    morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    config = require('./config'),
-    listingsRouter = require('../routes/listings.server.routes'), 
-    getCoordinates = require('../controllers/coordinates.server.controller.js');
+var path = require('path'), express = require('express'), mongoose = require('mongoose'), morgan = require('morgan'),
+    bodyParser = require('body-parser'), config = require('./config'),
+    listingsRouter = require('../routes/listings.server.routes'),
+    getCoordinates = require('../controllers/coordinates.server.controller.js'),
+
 
 module.exports.init = function() {
-  //connect to database
-  mongoose.connect(config.db.uri);
+    // connect to database
+    mongoose.connect(config.db.uri);
 
-  //initialize app
-  var app = express();
+    // initialize app
+    var app = express();
 
-  //enable request logging for development debugging
-  app.use(morgan('dev'));
+    // enable request logging for development debugging
+    app.use(morgan('dev'));
 
-  //body parsing middleware 
-  app.use(bodyParser.json());
+    // body parsing middleware
+    app.use(bodyParser.json());
 
-  /* server wrapper around Google Maps API to get latitude + longitude coordinates from address */
-  app.post('/api/coordinates', getCoordinates, function(req, res) {
-    res.send(req.results);
-  });
+    /* server wrapper around Google Maps API to get latitude + longitude coordinates from address */
+    app.post('/api/coordinates', getCoordinates, function(req, res) { res.send(req.results); });
+    
+    /* serve static files */
+    app.use('/', express.static('client'));
 
-  /* serve static files */
-  
+    /* use the listings router for requests to the api */
+    app.use('/api/listings', listingsRouter);
 
-  /* use the listings router for requests to the api */
+    /* go to homepage for all routes not specified */
+    app.all('/*', function(req, res, next) {
+        res.redirect('../index.html');
+    });
 
-
-  /* go to homepage for all routes not specified */ 
-
-  return app;
-};  
+    return app;
+};
--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -1,83 +1,134 @@
 
 /* Dependencies */
-var mongoose = require('mongoose'), 
-    Listing = require('../models/listings.server.model.js');
+var mongoose = require('mongoose'), Listing = require('../models/listings.server.model.js');
 
 /*
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
-  On an error you should send a 404 status code, as well as the error message. 
+  On an error you should send a 404 status code, as well as the error message.
   On success (aka no error), you should send the listing(s) as JSON in the response.
 
-  HINT: if you are struggling with implementing these functions, refer back to this tutorial 
+  HINT: if you are struggling with implementing these functions, refer back to this tutorial
   from assignment 3 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
  */
 
 /* Create a listing */
 exports.create = function(req, res) {
 
-  /* Instantiate a Listing */
-  var listing = new Listing(req.body);
+    /* Instantiate a Listing */
+    var listing = new Listing(req.body);
 
-  /* save the coordinates (located in req.results if there is an address property) */
-  if(req.results) {
-    listing.coordinates = {
-      latitude: req.results.lat, 
-      longitude: req.results.lng
-    };
-  }
-
-  /* Then save the listing */
-  listing.save(function(err) {
-    if(err) {
-      console.log(err);
-      res.status(400).send(err);
-    } else {
-      res.json(listing);
+    /* save the coordinates (located in req.results if there is an address property) */
+    if (req.results)
+    {
+        listing.coordinates = {latitude: req.results.lat, longitude: req.results.lng};
     }
-  });
+
+    /* Then save the listing */
+    listing.save(function(err) {
+        if (err)
+        {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        else
+        {
+            res.json(listing);
+        }
+    });
 };
 
 /* Show the current listing */
 exports.read = function(req, res) {
-  /* send back the listing as json from the request */
-  res.json(req.listing);
+
+    /* send back the listing as json from the request */
+    res.json(req.listing);
 };
 
 /* Update a listing */
 exports.update = function(req, res) {
-  var listing = req.listing;
+    var listing = req.listing;
 
-  /* Replace the article's properties with the new properties found in req.body */
-  /* save the coordinates (located in req.results if there is an address property) */
-  /* Save the article */
+    /* Replace the article's properties with the new properties found in req.body */
+    var code = req.body.code;
+    var name = req.body.name;
+    var address = req.body.address;
+
+    /* save the coordinates (located in req.results if there is an address property) */
+    var coordinates = null;
+    if(address !== null)
+    {
+        coordinates = req.results;
+    }
+
+    /* Save the article */
+    Listing.findOneAndUpdate({"name": name }, 
+    { $set: { "address": address, "name": name, "code": code, "coordinates": coordinates}},
+    {new: true},
+    function(err, doc){
+        if(err){
+            console.log("Something wrong when updating data!");
+        }
+        else{
+            console.log(doc);
+            res.json(doc);
+        }
+    });
 };
 
 /* Delete a listing */
 exports.delete = function(req, res) {
-  var listing = req.listing;
+    var listing = req.listing;
+    query = {code: listing.code};
 
-  /* Remove the article */
+    Listing.find(query, function(err, docs) {
+        if (err){ 
+            console.log(err);
+            res.status(400).send(err);
+        }
+    }).remove(function(err, docs){
+        if(err)
+        {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        else
+        {
+            res.json(listing);
+        }
+    });
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
-  /* Your code here */
+    Listing.find().sort({code: 1}).exec(function(err, docs) {
+        if (err)
+        {
+            res.status(400).send(err);
+        }
+        else
+        {
+            res.json(docs);
+        }
+    });
 };
 
-/* 
-  Middleware: find a listing by its ID, then pass it to the next request handler. 
+/*
+  Middleware: find a listing by its ID, then pass it to the next request handler.
 
-  HINT: Find the listing using a mongoose query, 
-        bind it to the request object as the property 'listing', 
+  HINT: Find the listing using a mongoose query,
+        bind it to the request object as the property 'listing',
         then finally call next
  */
 exports.listingByID = function(req, res, next, id) {
-  Listing.findById(id).exec(function(err, listing) {
-    if(err) {
-      res.status(400).send(err);
-    } else {
-      req.listing = listing;
-      next();
-    }
-  });
+    Listing.findById(id).exec(function(err, listing) {
+        if (err)
+        {
+            res.status(400).send(err);
+        }
+        else
+        {
+            req.listing = listing;
+            next();
+        }
+    });
 };
